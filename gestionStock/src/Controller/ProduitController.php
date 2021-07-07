36575441de7e394ebe8136b6005a52a64b17e5ad
hamzatebib <hamzatebib99@gmail.com>
2021-07-07 16:42:38 +0100

    commit1

---
+++ gestionStock/src/Controller/ProduitController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use App\Form\ProduitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ProduitController extends AbstractController
+{
+    /**
+     * @Route("/produit", name="produit")
+     */
+    public function index(): Response
+    {
+        return $this->render('produit/index.html.twig', [
+            'controller_name' => 'ProduitController',
+        ]);
+    }
+    /**
+     * @Route("/produit/new", name="produit_new")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $produit = new Produit();
+        $form = $this->createForm(ProduitType::class, $produit);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($produit);
+            $em->flush();
+
+            return $this->redirectToRoute('produit/new.html.twig');
+        }
+        return $this->render('produit/new.html.twig', [
+            "formProduit" => $form->createView()
+        ]);
+    }
+}
